dab13a3fc7568e2a92ae7cefbbddeba78f0735f4
Subarik <subarikosnova@gmail.com>
2024-02-13 18:09:56 +0200

    fich: Slider

--- frontend/src/components/MainComp/Main.jsx
+++ frontend/src/components/MainComp/Main.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Style from './Main.module.scss'
 import Card from '../CardComp/Card'
+import SimpleSlider from "../Slider/Slider";
+import FirstBanner from "../Banner/FirstBanner/FirstBanner";
+import SecondBanner from "../Banner/SecondBanner/SecondBanner";
 
 
 const Main = () => {
     return (
-        <main className={Style.main}>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+        <main className={Style.mainWrapper} >
+            <SimpleSlider/>
+            <div className={Style.main}>
+
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
         </main>
     );
 };
