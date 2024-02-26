d7d9c3bfec5a82b9edfe6fa51bf360fe5bdd71bd
Serhii Kichuk <sergeykichukki4@gmail.com>
2024-02-26 23:42:11 +0100

    chore(57): add ColorCircle component

---
+++ frontend/src/Components/ColorCircle/ColorCircle.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import styles from "./ColorCircle.module.scss";
+
+const ColorCircle = ({
+  hexColor,
+  pathname,
+  capacity,
+  color,
+  isActive,
+  changeColor,
+}) => {
+  return (
+    <Link
+      to={`${pathname}?color=${color}&capacity=${capacity}`}
+      onClick={() => changeColor(color)}
+    >
+      <div
+        className={`${styles.circle} ${isActive ? styles.active : ""}`}
+        style={{ backgroundColor: hexColor }}
+      ></div>
+    </Link>
+  );
+};
+
+ColorCircle.propTypes = {
+  hexColor: PropTypes.string.isRequired,
+  pathname: PropTypes.string.isRequired,
+  capacity: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  changeColor: PropTypes.func.isRequired,
+};
+
+export default ColorCircle;
