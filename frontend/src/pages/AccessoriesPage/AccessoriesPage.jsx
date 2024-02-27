3c3f6ddb6215fb534e4b49782b593846ef0b1948
Vladimir Subora <subarikosnova@gmail.com>
2024-02-27 10:37:34 +0000

    chore(60): add wrapper and accessories card

--- frontend/src/pages/AccessoriesPage/AccessoriesPage.jsx
+++ frontend/src/pages/AccessoriesPage/AccessoriesPage.jsx
@@ -1,23 +1,12 @@
 import React from "react";
-import Style from "../HomePage/HomePage.module.scss";
+import Style from "./AccessoriesPage.module.scss";
 
 const AccessoriesPage = () => {
   return (
     <div>
       <section className={Style.container}>
-        <h2>This is Accessories Page</h2>
-        <p>
-          Explore our vast array of accessories: from practical cases and
-          stylish headphones to innovative gadgets. Find the perfect complement
-          for your device and express your style with our high-quality
-          accessories. Whether you looking for protection, convenience, or a
-          touch of flair, we have something for everyone. Shop now and enhance
-          your tech experience with our diverse selection of accessories
-          tailored to suit your needs and preferences. With top brands and the
-          latest trends, we your one-stop destination for all things
-          accessories. Elevate your devices and make a statement with our
-          extensive collection today!
-        </p>
+        <h1>Accessories</h1>
+        <div className={Style.cardWrapper}>{/*<AccessoriesCard />*/}</div>
       </section>
     </div>
   );
