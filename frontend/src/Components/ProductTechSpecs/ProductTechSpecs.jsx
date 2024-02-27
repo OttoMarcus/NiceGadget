1d8795207b8ed6f205dc24b0baa6067b6b238037
Serhii Kichuk <sergeykichukki4@gmail.com>
2024-02-27 02:52:48 +0100

    feat(57): add ProductTechSpecs component

---
+++ frontend/src/Components/ProductTechSpecs/ProductTechSpecs.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./ProductTechSpecs.module.scss";
+const TechSpecs = ({ techSpecs, capacity }) => {
+  return (
+    <div>
+      {techSpecs.map((spec, index) => (
+        <div key={index} className={styles.techSpec}>
+          <div className={styles.specName}>{spec.specName}</div>
+          <div className={styles.specDescription}>
+            {spec.specName === "Built in memory"
+              ? capacity
+              : spec.specDescription}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+TechSpecs.propTypes = {
+  capacity: PropTypes.string.isRequired,
+  techSpecs: PropTypes.arrayOf(
+    PropTypes.shape({
+      specName: PropTypes.string.isRequired,
+      specDescription: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+export default TechSpecs;
