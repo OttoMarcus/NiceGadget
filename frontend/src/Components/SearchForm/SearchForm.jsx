acdd52d0d67b35140b871c0ebdbd46a137f95f6b
vadym benedyk <o.oo.o@zoho.com>
2024-03-11 01:19:32 +0200

    (68) - Search field

--- frontend/src/Components/SearchForm/SearchForm.jsx
+++ frontend/src/Components/SearchForm/SearchForm.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchLogo from "../Icons/SearchLogo";
-
 import styles from "./SearchForm.module.scss";
 
 const SearchForm = () => {
+  const [showInput, setShowInput] = useState(false);
+
+  const handleShow = () => {
+    setShowInput(true);
+  };
+  const handleBlur = () => {
+    setShowInput(false);
+  };
+
   return (
-    <form action="" className={styles.searchForm}>
-      <input
-        type="text"
-        placeholder="Search ..."
-        className={styles.searchInput}
-      />
-      <SearchLogo />
+    <form
+      action=""
+      onClick={handleShow}
+      onMouseOver={handleShow}
+      onBlur={handleBlur}
+      className={styles.searchForm}
+    >
+      <SearchLogo isActive={showInput} />
+      {showInput && (
+        <input
+          type="text"
+          placeholder="Search ..."
+          className={styles.searchInput}
+        />
+      )}
     </form>
   );
 };
