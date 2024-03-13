c0a92320cec97c59c8553e82a5e2f4af5d83256d
Otto Marcus <benedyk.v@donnu.edu.ua>
2024-03-13 15:34:46 +0000

    (68) - Search page and grid products layout

--- frontend/src/Components/SearchForm/SearchForm.jsx
+++ frontend/src/Components/SearchForm/SearchForm.jsx
@@ -1,23 +1,41 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {useDispatch} from "react-redux";
+import { getSearchList } from "../../store/search/searchSlice";
 import SearchLogo from "../Icons/SearchLogo";
+
 import styles from "./SearchForm.module.scss";
+
 
 const SearchForm = () => {
   const [showInput, setShowInput] = useState(false);
-  const [value, setValue] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const handleShow = () => {
     setShowInput(true);
+    navigate("/search");
+
   };
   const handleBlur = () => {
     setShowInput(false);
   };
 
+  const handleInputChange = (event) => {
+    dispatch(getSearchList(event.target.value));
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <form
-      action=""
-      onClick={handleShow}
-      // onMouseOver={handleShow}
+      onSubmit={handleSubmit}
+      onMouseOver={handleShow}
+      onMouseOut={handleBlur}
       onBlur={handleBlur}
+      onFocus={handleShow}
       className={styles.searchForm}
     >
       <SearchLogo isActive={showInput} />
@@ -26,7 +44,7 @@ const SearchForm = () => {
           type="text"
           placeholder="Search ..."
           className={styles.searchInput}
-          onChange={(event) => setValue(event.target.value)}
+          onChange={handleInputChange}
         />
       )}
     </form>
