65990fcd6a7f702652c6ad527a38bc3733af02f9
ОН-з21 Столяров Данііл Максимович <iamstolyarovdanya@gmail.com>
2024-03-17 00:16:20 +0200

    feat(87): favor back

--- backend/models/Wishlist.js
+++ backend/models/Wishlist.js
@@ -5,10 +5,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WishlistSchema = new Schema({
-        // _id:{
-        //     type: Schema.Types.ObjectId,
-        //     require:  false
-        // },
     id: {
         type : String
     },
