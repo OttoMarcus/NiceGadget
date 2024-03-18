df8e8210a311b7618ee14ac33b1ab479d3baa9db
Otto Marcus <benedyk.v@donnu.edu.ua>
2024-03-18 10:40:48 +0000

    (68) Search

--- frontend/src/Components/SearchForm/SearchForm.jsx
+++ frontend/src/Components/SearchForm/SearchForm.jsx
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getSearchList } from "../../store/search/searchSlice";
 import SearchLogo from "../Icons/SearchLogo";
+
 import styles from "./SearchForm.module.scss";
 
 const SearchForm = () => {
   const [showInput, setShowInput] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleShow = () => {
     setShowInput(true);
+    navigate("/search");
   };
   const handleBlur = () => {
     setShowInput(false);
   };
 
+  const handleInputChange = (event) => {
+    dispatch(getSearchList(event.target.value));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <form
-      action=""
-      onClick={handleShow}
-      // onMouseOver={handleShow}
+      onSubmit={handleSubmit}
+      onMouseOver={handleShow}
+      onMouseOut={handleBlur}
       onBlur={handleBlur}
+      onFocus={handleShow}
       className={styles.searchForm}
     >
       <SearchLogo isActive={showInput} />
@@ -26,6 +42,7 @@ const SearchForm = () => {
           type="text"
           placeholder="Search ..."
           className={styles.searchInput}
+          onChange={handleInputChange}
         />
       )}
     </form>
