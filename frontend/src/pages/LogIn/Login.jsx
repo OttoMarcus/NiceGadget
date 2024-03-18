fe791c348f4b272e2f583c358981b0c87f7abb29
Serhii Osadchuk <osadchuksv.ne@gmail.com>
2024-03-18 05:37:08 +0200

    quick-fix: eslint requirments for unused import

--- frontend/src/pages/LogIn/Login.jsx
+++ frontend/src/pages/LogIn/Login.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styles from "./Login.module.scss";
 import UserLoginForm from "../../Components/Forms/AuthorizationForm/UserLoginForm";
 
 const Login = () => {
   return (
     <div className={`${styles.container}${styles.loginPageWrapper}`}>
-      {/* <h1 className={Style.tittle}>This is Login Page </h1>
-      <Link className={Style.linksBtn} to="/">
-        Home
-      </Link> */}
       <UserLoginForm />
     </div>
   );
