e924b066c09642d79845f197ca196e839b5a0a5c
Serhii Osadchuk <osadchuksv.ne@gmail.com>
2024-03-18 05:39:25 +0200

    quick-fix: eslint requirments for unused import

--- frontend/src/pages/Registration/Registration.jsx
+++ frontend/src/pages/Registration/Registration.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styles from "./Registration.module.scss";
 import UserRegForm from "../../Components/Forms/UserRegFrom/UserRegForm";
 
 const Registration = () => {
   return (
     <div className={`${styles.container}${styles.perPageWrapper}`}>
-      {/* <h1 className={style.tittle}>This is Registration Page </h1> */}
-      {/* <Link className={style.linksBtn} to="/">
-        Home
-      </Link> */}
       <UserRegForm />
     </div>
   );
