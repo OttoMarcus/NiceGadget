c673651c34d0c1f54e05633d0974ae82acefc253
Olga Novyk <helga.novyk@gmail.com>
2024-03-18 20:15:09 +0100

    fix(13): fix for CardAccessories location

--- frontend/src/Components/CardAccessories/CardAccessories.jsx
+++ frontend/src/Components/CardAccessories/CardAccessories.jsx
@@ -1,89 +1,53 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
-import Button from "../Button/Button";
+import styles from "./CardAccessories.module.scss";
 import Favorite from "../Favorite/Favorite";
+import { useDispatch, useSelector } from "react-redux";
 import { Tooglefavorites } from "../../store/favorites/favoriteSlice";
-import { addToCartLocal } from "../../store/cart/cartSlice";
-import { addToCartServer } from "../../API/cartAPI";
-
-import styles from "./Card.module.scss";
+import CartButton from "../CartButton/CartButton";
 
 const CardAccessories = (props) => {
-  const {
-    _id,
-    id,
-    picture,
-    name,
-    price,
-    category,
-    color,
-    available,
-    size,
-    weight,
-  } = props;
+  const { id, name, picture, price, color, size, weight, category } = props;
   const dispatch = useDispatch();
-  const isAuthorized = useSelector((state) => state.user.isAuthorized);
 
   const favor = useSelector((state) => state.favorite.favorites);
   const some = favor.some((el) => id === el.id);
 
+  const productToAdd = { ...props };
   const cartItems = useSelector((state) => state.cart.cartItems);
-  const inCart = cartItems.some((item) => item.productId === _id);
-  const isAvailable = available;
-  const backgroundColorBtn = isAvailable && !inCart ? "#905BFF" : "#323542";
-
-  const handleAddToCart = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-
-    const productToAdd = { ...props };
-
-    if (isAuthorized) {
-      dispatch(addToCartServer(productToAdd));
-    } else {
-      dispatch(addToCartLocal({ productToAdd }));
-    }
-  };
+  const inCart = cartItems.some((item) => item.productId === productToAdd._id);
 
   return (
     <Link to={`/${category}/${name}?color=${color}`}>
       <div className={styles.card}>
-        <div className={`${styles.cardImg} ${styles.imageWrapper}`}>
-          <img src={picture} className={styles.innerImg} alt="Card" />
+        <div className={styles.cardImg}>
+          <img src={picture} alt="Card" />
         </div>
-        <div className={styles.nameWrapper}>
-          <div className={styles.model}>{name}</div>
-        </div>
+        <div className={styles.model}>{name}</div>
         <div className={styles.price}>${price}</div>
         <div className={styles.divider}></div>
         <ul className={styles.paramsGroup}>
           <li>
-            <p>Size</p>
+            <p>Size:</p>
             <p>{size}</p>
           </li>
           <li>
-            <p>Color</p>
+            <p>Color:</p>
             <p>{color}</p>
           </li>
           <li>
-            <p>Weight</p>
+            <p>Weight:</p>
             <p>{weight}</p>
           </li>
         </ul>
         <div className={styles.buttonWrapper}>
-          <Button
-            onClick={(event) => handleAddToCart(event)}
-            backgroundColor={backgroundColorBtn}
-          >
-            {isAvailable
-              ? inCart
-                ? "Added to cart"
-                : "Add to cart"
-              : "Notify when available"}
-          </Button>
+          <CartButton
+            productToAdd={productToAdd}
+            isAvailable={productToAdd?.available}
+            inCart={inCart}
+            fetchDetailsUrl={null}
+          />
 
           <Favorite
             click={(event) => {
@@ -100,7 +64,7 @@ const CardAccessories = (props) => {
 };
 
 CardAccessories.propTypes = {
-  _id: PropTypes.string,
+  _id: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   picture: PropTypes.string.isRequired,
