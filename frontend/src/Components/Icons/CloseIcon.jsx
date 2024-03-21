be732748958c01d7761859179c32060178214a25
Yaroslav Khlopchur <klopchur03@gmail.com>
2024-03-21 09:54:40 +0000

    feat(98): editing data profile, styling user profile

--- frontend/src/Components/Icons/CloseIcon.jsx
+++ frontend/src/Components/Icons/CloseIcon.jsx
@@ -4,8 +4,8 @@ const CloseIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
+      width="19"
+      height="19"
       viewBox="0 0 16 16"
       fill="none"
     >
