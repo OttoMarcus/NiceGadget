06188446f2487e1f7d9ae04e5876e845209d56b0
Serhii Kichuk <sergeykichukki4@gmail.com>
2024-03-27 22:43:25 +0100

    fix(135): test Sentry`s Error Boundary

--- frontend/src/Components/Cards/Card.jsx
+++ frontend/src/Components/Cards/Card.jsx
@@ -72,6 +72,13 @@ const Card = (props) => {
             <p>{ram}</p>
           </li>
         </ul>
+        <button
+          onClick={() => {
+            throw new Error("Break");
+          }}
+        >
+          Break the world
+        </button>
         <div className={styles.buttonWrapper}>
           <CartButton
             productToAdd={productToAdd}
