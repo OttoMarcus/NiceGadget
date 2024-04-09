5108a34e236b5e5637146ed706c6fcf0a960fb4a
ОН-з21 Столяров Данііл Максимович <iamstolyarovdanya@gmail.com>
2024-04-09 23:11:14 +0000

    Style orders

--- frontend/src/Components/Order/Order.jsx
+++ frontend/src/Components/Order/Order.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types"; // Import PropTypes
+import PropTypes from "prop-types";
 import style from "./Order.module.scss";
+import DownArrowIconOrders from "../Icons/DownArrowIconOrders";
 
 const Order = ({
   _id,
@@ -25,96 +26,102 @@ const Order = ({
   const day = dateObject.getDate();
   const month = dateObject.getMonth() + 1;
   const year = dateObject.getFullYear();
-  const hours = dateObject.getHours();
-  const minutes = dateObject.getMinutes();
-  // const seconds = dateObject.getSeconds();
+
   const handelToggle = () => {
     setToggleOpen(!toggleOpen);
   };
   return (
-    <div className={style.container}>
-      <div className={style.containerOrder}>
-        <div className={style.infoSectionTitle}>
-          <div className={style.orderNumm}>
-            <p className={style.info}>Order number:</p>
-            <p className={style.subInfo}>{orderNo}</p>
-          </div>
-
-          <p
-            className={style.toggleProducts}
-            onClick={() => handelToggle(orderNo)}
-          >
-            {toggleOpen ? `Close All` : `Open All`}
-          </p>
+    <div className={`${style.container} ${style.marginBtm}`}>
+      <div className={style.order}>
+        <div className={style.orderWrapper}>
+          <p className={style.orderNumberTitle}>Order Number:</p>
+          <p className={style.orderNumberSubTitle}>{orderNo}</p>
         </div>
-        {toggleOpen && (
-          <>
-            <div className={style.infoSection}>
-              <div className={style.infoWrapper}>
-                <p className={style.info}>User:</p>
-                <p className={style.subInfo}>
-                  {userFirstName} {userLastName}
-                </p>
-                <p className={style.info}>Delivery address:</p>
-                <p className={style.subInfo}> {deliveryAddress}</p>
-                {/*<p className={style.info}>Total sum: {totalSum}$</p>*/}
-                <p className={style.info}>Delivery Method: {deliveryMethod}</p>
-                <p className={style.info}>Payment Method: {paymentMethod}</p>
-                <p className={style.info}>Status: {status}</p>
-                <p className={style.info}>Email: {email}</p>
-                <p className={style.info}>Mobile: {mobile}</p>
-                <p className={style.info}>
-                  Date: {day}.{month}.{year}
-                </p>
-                <p className={style.info}>
-                  Time: {hours}:{minutes}{" "}
+        <div>
+          <button onClick={handelToggle} className={style.orderButton}>
+            {toggleOpen ? `Close All` : `Open All`}
+            <DownArrowIconOrders />
+          </button>
+        </div>
+      </div>
+      {toggleOpen && (
+        <>
+          <div className={style.openOrderSection}>
+            <div className={style.infoWrapper}>
+              <div className={style.infoTextWrapper}>
+                <p className={style.infoText}>Name: </p>
+                <p className={style.infoTextSecond}>{userFirstName} </p>
+              </div>
+              <div className={style.infoTextWrapper}>
+                <p className={style.infoText}>LastName: </p>
+                <p className={style.infoTextSecond}>{userLastName} </p>
+              </div>
+
+              <div className={style.infoTextWrapper}>
+                <p className={style.infoText}>Pay Method: </p>
+                <p className={style.infoTextSecond}>{paymentMethod} </p>
+              </div>
+              <div className={style.infoTextWrapper}>
+                <p className={style.infoText}>Delivery Address: </p>
+                <p className={style.infoTextSecond}>{deliveryAddress} </p>
+              </div>
+              <div className={style.infoTextWrapper}>
+                <p className={style.infoText}>Delivery Method: </p>
+                <p className={style.infoTextSecond}>{deliveryMethod} </p>
+              </div>
+              <div className={style.infoTextWrapper}>
+                <p className={style.infoText}>Email: </p>
+                <p className={style.infoTextSecond}>{email}</p>
+              </div>
+              <div className={style.infoTextWrapper}>
+                <p className={style.infoText}>Data: </p>
+                <p className={style.infoTextSecond}>
+                  {day}.{month}.{year}
                 </p>
               </div>
-              <div className={style.price}>
-                <h2>Total price: </h2>
-                <p>{totalSum}$</p>
+              <div className={style.infoTextWrapper}>
+                <p className={style.infoText}>Price: </p>
+                <p className={style.infoTextSecond}>{totalSum} </p>
               </div>
+              <div className={style.infoTextWrapper}>
+                <p className={style.infoText}>Mobile: </p>
+                <p className={style.infoTextSecond}>{mobile} </p>
+              </div>
+              <p className={style.progress}>{status}</p>
             </div>
-            <div className={style.pictureSection}>
-              {products.map((product) => {
-                const discountedPrice = product.discount
-                  ? Math.round(product.price - product.price * product.discount)
-                  : product.price;
-                const percentDiscount = product.discount
-                  ? product.discount * 100
-                  : 0;
+            {products.map((product, index) => {
+              const discountedPrice = product.discount
+                ? Math.round(product.price - product.price * product.discount)
+                : product.price;
 
-                return (
-                  <div key={product.productId} className={style.product}>
-                    <img
-                      className={style.img}
-                      src={product.picture}
-                      alt={product.name}
-                    />
-                    <div className={style.subInfoWrapper}>
-                      <p className={style.info}>Name:</p>
-                      <p className={`${style.subInfo}`}>{product.name}</p>
-
-                      <p className={style.info}>Price:</p>
-                      <p className={style.subInfo}>
-                        {discountedPrice}${" "}
-                        {product.discount > 0 && (
-                          <span className={style.discount}>
-                            {" "}
-                            (-{percentDiscount}%)
-                          </span>
-                        )}
-                      </p>
-                      <p className={style.info}>Amount:</p>
-                      <p className={style.subInfo}>1</p>
+              return (
+                <div key={index} className={style.imgWrapper}>
+                  <div className={style.cartItem}>
+                    <div className={style.cartItemInfo}>
+                      <div className={style.cartItemImg}>
+                        <img src={product.picture} alt="231" />
+                      </div>
+                      <div className={style.cartItemName}>{product.name}</div>
+                    </div>
+                    <div className={style.counterPriceWrapper}>
+                      <div className={style.counter}>
+                        <span className={style.quantity}>
+                          Quantity: {product.cartQuantity}
+                        </span>
+                      </div>
+                      <div className={style.priceWrapper}>
+                        <h3 className={style.totalItemPrice}>
+                          Price: {discountedPrice}$
+                        </h3>
+                      </div>
                     </div>
                   </div>
-                );
-              })}
-            </div>
-          </>
-        )}
-      </div>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 };
